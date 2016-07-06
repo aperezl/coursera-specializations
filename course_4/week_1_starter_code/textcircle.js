06ac2ffc543f90ccb69edf0f050603ab0b4396d1
Antonio Manuel Pérez López <aperezl@gmail.com>
2016-07-06 13:16:01 +0200

    course 4 - week 1

---
+++ course_4/week_1_starter_code/textcircle.js
@@ -0,0 +1,33 @@
+this.Documents = new Mongo.Collection('documents');
+
+if (Meteor.isClient){
+
+	Template.editor.helpers({
+		docid: function() {
+			var doc = Documents.findOne();
+			if(doc) {
+				return Documents.findOne()._id;
+			} else {
+				return undefined;
+			}
+		},
+		config: function() {
+			return function(editor) {
+				editor.on('change', function(cm_editor, info) {
+					console.log(cm_editor.getValue());
+					$('#viewer_iframe').contents().find('html').html(cm_editor.getValue());
+				})
+				console.log(editor);
+			}
+		}
+	});
+}
+
+if (Meteor.isServer){
+	Meteor.startup(function(){
+		// code to run on server at startup
+		if(!Documents.findOne()) {
+			Documents.insert({title: 'my new document'});
+		}
+	})
+}
